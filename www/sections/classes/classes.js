995c96c5c50947ea2b615d05dd4f0fed6b7714fb
darkcooger <cooger@darkcooger.net>
2015-02-21 13:36:56 -0600

    Refactor

--- www/sections/classes/classes.js
+++ www/sections/classes/classes.js
@@ -3,88 +3,62 @@ angular.module("jotc")
 	{
 		$scope.classes = $api.classes.list;
 		$scope.classTypes = $api.classes.types;
+		
+		var getClassTypesList = function(clss, advanced)
+		{
+			var str = "";
+			var numListed = 0;
+			
+			for(var i = 0; i < clss.classTypes.length; i++)
+			{
+				if(clss.classTypes[i].isAdvanced === advanced)
+				{
+					if(numListed > 0)
+					{
+						if(i === clss.classTypes.length - 1)
+						{
+							if(numListed > 1)
+							{
+								str += ",";
+							}
+							str += " and ";
+						}
+						else
+						{
+							str += ", ";
+						}
+					}
+
+					str += clss.classTypes[i].name;
+					numListed++;
+				}
+			}
+			
+			return {
+				string: str,
+				count: numListed
+			};
+		};
 
 		$scope.getDescriptionBlocks = function(clss)
 		{
 			var descriptions = [ ];
 			
-			var basicStr = "";
-			var basicClassesListed = 0;
-			for(var i = 0; i < clss.classTypes.length; i++)
-			{
-				console.log(clss.classTypes[i]);
-				
-				if(!clss.classTypes[i].isAdvanced)
-				{
-					if(basicClassesListed > 0)
-					{
-						if(i === clss.classTypes.length - 1)
-						{
-							if(basicClassesListed > 1)
-							{
-								basicStr += ",";
-							}
-							basicStr += " and ";
-						}
-						else
-						{
-							basicStr += ", ";
-						}
-					}
-					
-					console.log(clss.classTypes[i]);
-
-					basicStr += clss.classTypes[i].name;
-					basicClassesListed++;
-				}
-			}
-			
 			var day = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ][new Date(clss.startDate).getUTCDay()];
-			if(basicClassesListed > 0)
-			{
-				basicStr += " will be offered.  Classes meet every " + day + " at " + clss.timeOfDay + " for " + clss.hoursPerWeek + " hour" + (clss.hoursPerWeek === 1 ? "" : "s") + " and last " + clss.numberOfWeeks + " weeks, including a graduation night.";
-			}
-
+			
+			var basicStr = getClassTypesList(clss, false).string;
 			if(basicStr.length > 0)
 			{
+				basicStr += " will be offered.  Classes meet every " + day + " at " + clss.timeOfDay + " for " + clss.hoursPerWeek + " hour" + (clss.hoursPerWeek === 1 ? "" : "s") + " and last " + clss.numberOfWeeks + " weeks, including a graduation night.";
 				descriptions.push(basicStr);
 			}
-			
-			var advancedStr = "";
-			var advancedClassesListed = 0;
-			for(var i = 0; i < clss.classTypes.length; i++)
-			{
-				if(clss.classTypes[i].isAdvanced)
-				{
-					if(advancedClassesListed > 0)
-					{
-						if(i === clss.classTypes.length - 1)
-						{
-							if(advancedClassesListed > 1)
-							{
-								advancedStr += ",";
-							}
-							advancedStr += " and ";
-						}
-						else
-						{
-							advancedStr += ", ";
-						}
-					}
 
-					advancedStr += clss.classTypes[i].name;
-					advancedClassesListed++;
-				}
-			}
-
-			var requires = (advancedClassesListed > 1 ? "These classes require" : "This class requires");
-			if(advancedClassesListed > 0)
-			{
-				advancedStr += " will " + (basicStr.length > 0 ? "also " : "") + "be available if enough dogs enroll.  " + requires + " that the dog previously completed the JOTC Basic Obedience course.";
-			}
-			
+			var advanced = getClassTypesList(clss, true);
+			var advancedStr = advanced.string;
 			if(advancedStr.length > 0)
 			{
+				var requires = (advanced.count > 1 ? "These classes require" : "This class requires");
+				advancedStr += " will " + (basicStr.length > 0 ? "also " : "") + "be available if enough dogs enroll.  " + requires + " that the dog previously completed the JOTC Basic Obedience course.";
 				descriptions.push(advancedStr);
 			}
 
