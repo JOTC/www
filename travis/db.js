f64a8a4a5853b55f3488e99d2b8a90372345fd0d
darkcooger <cooger@darkcooger.net>
2015-02-21 20:13:35 -0600

    Travis

---
+++ travis/db.js
@@ -0,0 +1,9 @@
+// "test-password-1"
+// $2a$10$YZR8NMyyDzFY5ixvNerUneTr/2qGkxVgi.uzGBQxhv9koEj//6zrK
+//
+// "test-password-2"
+// $2a$10$X/zlr3SzwNgTnKLs/YztQeZxbxTJwy0GZelyJPrrjbFIzyRFvm9Z2
+
+use jotc
+db.users.insert({ name: "Test User 1", email: "", local: { username: "test1", password: "$2a$10$YZR8NMyyDzFY5ixvNerUneTr/2qGkxVgi.uzGBQxhv9koEj//6zrK" }, permissions: { "links": false, "officers": false, "shows": false, "classes": false, "pictures": false, "calendar": false, "users": false }});
+db.users.insert({ name: "Test User 2", email: "", local: { username: "test2", password: "$2a$10$X/zlr3SzwNgTnKLs/YztQeZxbxTJwy0GZelyJPrrjbFIzyRFvm9Z2" }, permissions: { "links": true, "officers": true, "shows": true, "classes": true, "pictures": true, "calendar": true, "users": true }});
